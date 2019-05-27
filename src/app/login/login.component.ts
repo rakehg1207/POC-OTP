937d9ff4b4ac60690d7c212e3c1de0d59fc469ea
Rakesh G <rakeshgachar07@gmail.com>
2019-05-27 20:18:18 +0530

    ->login page form val changes. ->home page using flex for body content. ->separate footer component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ElementRef } from '@angular/core';
+import { Component, OnInit,ElementRef ,ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
 // import { Routes, RouterModule } from '@angular/router';
 import { LoginConfig} from './login.constant';
@@ -10,14 +10,16 @@ import { LoginConfig} from './login.constant';
   providers: [ LoginConfig]
 })
 export class LoginComponent implements OnInit {
-
+  @ViewChild('f') signupForm : NgForm;
+  isValid : boolean = false;
   constructor(private _configuration: LoginConfig) { }
 
   ngOnInit() {
   }
 
-  onSubmit(form : NgForm){
-    console.log(form);
+  onSubmit(){
+    console.log(this.signupForm);
+    
   }
 
 }
