62217094f0b57a6bede33b31f9f106453a37cb26
Rakesh G <rakeshgachar07@gmail.com>
2019-05-30 16:51:09 +0530

    ->changing img to mat icons. ->changes in line chart.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ElementRef,ViewChild} from '@angular/core';
 import * as moment from 'moment';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
@@ -9,25 +9,14 @@ import { Color, Label } from 'ng2-charts';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  public  heroes :any = [];
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [30, 79, 20, 61, 46, 35, 72], label: 'Series b' },
- ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  // public lineChartOptions: (ChartOptions & { annotation: any }) = {
-  //   responsive: true,
-  // };
-  lineChartOptions: any = { legend: { display: true, labels: { fontColor: 'black' }}}
-  public lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
-  ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
-  public lineChartPlugins = [];
+  @ViewChild('myCanvas')
+  public canvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+  public chartType: string = 'line';
+  public chartData: any[];
+  public chartLabels: any[];
+  public chartColors: any[];
+  public chartOptions: any;
   
   constructor() { 
     
@@ -35,8 +24,58 @@ export class HomePageComponent implements OnInit {
 
  
   ngOnInit() {
+    this.chartData = [{
+      data: [3, 1, 4, 2, 5],
+      label: 'Anthracnose',
+      fill: false
+    },{
+      data: [2, 4, 1, 3, 6],
+      label: 'secondLine',
+      fill: false
+    }];
+    this.chartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
+    this.chartColors = [{
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+         borderColor: 'rgba(0, 0, 0, 1)'
+    }];
+    this.chartOptions = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1
+          }
+        }]
+      },
+      annotation: {
+         drawTime: 'beforeDatasetsDraw',
+         annotations: [{
+            type: 'box',
+            id: 'a-box-1',
+            yScaleID: 'y-axis-0',
+            yMin: 0,
+            yMax: 1,
+            backgroundColor: '#4cf03b'
+         }, {
+            type: 'box',
+            id: 'a-box-2',
+            yScaleID: 'y-axis-0',
+            yMin: 1,
+            yMax: 2.7,
+            backgroundColor: '#fefe32'
+         }, {
+            type: 'box',
+            id: 'a-box-3',
+            yScaleID: 'y-axis-0',
+            yMin: 2.7,
+            yMax: 5,
+            backgroundColor: '#fe3232'
+         }]
+      }
+    }
+  };
   }
 
  
 
-}
+
